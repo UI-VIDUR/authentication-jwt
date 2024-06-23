f84dc2588e0d00ae7bd7cd50fe1392472113b8fe
vidur <vidurtuli53@gmail.com>
2024-06-24 01:17:04 +0530

    user registration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios';
+
+import "./App.css";
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    fullname: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
+
+  const handleUserRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3001/api/v1/register', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      
+      if(response.status === 201){
+        console.log(response.data.data.token)
+        console.log(formData)
+      }
+      
+    } catch (error) {
+      console.log(error.response.data.error)
+    }
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleUserRegister}>
+        <div className="form-group">
+          <label htmlFor="fullname">Enter FullName</label>
+          <input 
+            type="text" 
+            name="fullname" 
+            id="fullname" 
+            value={formData.fullname} 
+            onChange={handleChange}   
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Enter Email</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email"
+            value={formData.email} 
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Enter Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            value={formData.password} 
+            onChange={handleChange} 
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Register</button>
+          <p>OR</p>
+          <button type="button">Login</button>
+        </div>
+      </form>
     </div>
   );
 }
